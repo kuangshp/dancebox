5bb664e7877388f81cb03eb52b67ae28016c1056
kuangshp <kuangshp@126.com>
2022-09-05 22:42:52 +0800

    feat: 修改模型

--- model/UserSegmentEntity.go
+++ model/UserSegmentEntity.go
@@ -2,12 +2,13 @@ package model
 
 type UserSegmentEntity struct {
 	BaseEntity
-	ActivityId      int64  `gorm:"activity_id" json:"activityId"`            // 关联活动表(activity)主键id
+	ActivityId      int64  `gorm:"activity_id" json:"activityId"`            // 关联活动表(numberSegment)主键id
 	NumberSegmentId int64  `gorm:"number_segment_id" json:"numberSegmentId"` // 关联抽号表(umber_segment)主键id
 	Index           int64  `gorm:"index" json:"index"`                       // 抽到号码数字
 	UserId          int64  `gorm:"user_id" json:"userId"`                    // 小程序用户id
 	NickName        string `gorm:"nick_name" json:"nickName"`                // 小程序用户昵称
 	Mobile          string `gorm:"mobile" json:"mobile"`                     // 小程序用户手机号码
+	OrderListId     int64  `gorm:"order_list_id" json:"orderListId"`         // 关联到订单详情表id
 }
 
 func (t *UserSegmentEntity) TableName() string {
